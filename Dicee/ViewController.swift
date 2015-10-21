8f9cb48a62172b8755788d2e8559e755a91839a4
angelabauer <angela_yu@hotmail.co.uk>
2015-10-21 12:07:14 +0100

    Starter Project

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -15,7 +15,9 @@ class ViewController: UIViewController {
     
     //TODO: Step 2 - Drop your imageView IBOutlets just below this line
     
+    
     //TODO: Step 4 - Declare an array called diceArray here.
+    
     
     //TODO: Step 5 - Declare 2 variables that are of type Int, called firstRandomIndex and secondRandomIndex
     
@@ -30,27 +32,28 @@ class ViewController: UIViewController {
     }
     
     
-    //TODO: Step 6 - Make a function called updateDiceImages() here.
+    //TODO: Step 6 - Make a method called updateDiceImages() here.
     
-        //TODO: Step 7 - Assign a random number between 0 and 5 to firstRandomIndex and secondRandomIndex
+    
+        //TODO: Step 7 - Inside the updateDiceImages method you just made: Assign a random number between 0 and 5 to firstRandomIndex and secondRandomIndex
         
-        //TODO: Step 8 - Set the image for firstDice by using the filename from the diceArray at the value of firstRandomIndex.
-        
-        
-        //TODO: Step 9 - Do the same for the secondDiceImageView with secondRandomIndex
+        //TODO: Step 8 - Inside updateDiceImages method: Set the image for firstDice by using the filename from the diceArray at the value of firstRandomIndex.
         
         
-    //+++++++++++++End of updateDiceImages()++++++++++++++
+        //TODO: Step 9 - Inside updateDiceImages: Do the same for the secondDiceImageView with secondRandomIndex
+        
+        
+    
  
     
     
     //TODO: Step 3 - Link up the "Roll" button to an IBAction called rollButtonPressed() just below this line.
     
     
-        //TODO: Step 10 - Call updateDiceImages() function in the rollButtonPressed()  method.
+        //TODO: Step 10 - Inside the rollButtonPressed method: Call updateDiceImages() function in the rollButtonPressed()  method.
         
     
-    //+++++++++++++End of rollButtonPressed()++++++++++++++
+    
     
  
     
@@ -61,7 +64,7 @@ class ViewController: UIViewController {
     override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
         if motion == .MotionShake {
             
-            //TODO: Step 11 - Call updateDiceImages() function
+            //TODO: Step 11 - Call updateDiceImages() method here
             
         }
         
